f69bc7e1f11ddc8f4c7ddecb49544776c95a771d
Nick Darvey <nick@two-bulls.com>
2017-11-26 15:47:54 +1100

    Initial commit

---
+++ LanguageExt.AspNetCore/InternalErrorResult.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageExt
+{
+    internal class InternalErrorResult : ObjectResult
+    {
+        public InternalErrorResult(object value) : base(value)
+        {
+            StatusCode = StatusCodes.Status500InternalServerError;
+        }
+    }
+}
---
+++ LanguageExt.AspNetCore/TryAsync.Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LanguageExt
+{
+    public static class TryAsync
+    {
+        public static Task<IActionResult> ToActionResult<T>(this TryAsync<T> self) =>
+            self.Match<T, IActionResult>(
+                Succ: r => new OkObjectResult(r),
+                Fail: ex => new InternalErrorResult(ex));
+    }
+}
